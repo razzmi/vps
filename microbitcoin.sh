f828f85496a1e244f8ce72b952a34938ff592866
razzmi <95491619+razzmi@users.noreply.github.com>
2021-12-03 16:07:15 +0000

    Update microbitcoin.sh

--- microbitcoin.sh
+++ microbitcoin.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz 
-./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u mbc1q98hfcejqtd44024lx3un0ssrnwumljwcjyh708.rosy -t4
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u mbc1q98hfcejqtd44024lx3un0ssrnwumljwcjyh708.rosy -p x --cpu4
