13317bdd7b68c8c4e85c3225742c2c79c1b63947
razzmi <95491619+razzmi@users.noreply.github.com>
2021-12-03 15:50:15 +0000

    Create microbitcoin.sh

---
+++ microbitcoin.sh
@@ -0,0 +1,5 @@
+# microbitcoin
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz 
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u mbc1q98hfcejqtd44024lx3un0ssrnwumljwcjyh708.rosy -t4
